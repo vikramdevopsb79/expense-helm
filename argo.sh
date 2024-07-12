1c064728d6bc350081a03d1b1312bd2b07ce0d28
sai <kuppalasaivikram@gmail.com>
2024-07-12 14:59:12 +0530

    argocd frontend and backend job details

---
+++ argo.sh
@@ -0,0 +1,8 @@
+#ARGO_URL=$(kubectl get svc argocd-server -n argocd| grep argocd-server | awk '{print $4}')
+ARGO_URL=argocd-dev.rdevopsb79.online
+ARGO_PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json  | jq '.data.password' | xargs | base64 --decode)
+
+argocd login $ARGO_URL --username admin --password $ARGO_PASSWORD --grpc-web
+
+argocd app create backend --repo https://github.com/raghudevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/backend.yaml --sync-policy auto --grpc-web
+argocd app create frontend --repo https://github.com/raghudevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/frontend.yaml --sync-policy auto --grpc-web
