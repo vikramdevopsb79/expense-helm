f21a7da49de8418f4d17affa741f05a177c89bce
sai <kuppalasaivikram@gmail.com>
2024-07-16 16:43:01 +0530

    ingress added to provide certificate and act as n/w load balancer

--- argo.sh
+++ argo.sh
@@ -3,6 +3,6 @@ ARGO_URL=argocd-dev.vikramdevops.tech
 ARGO_PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json  | jq '.data.password' | xargs | base64 --decode)
 
 argocd login $ARGO_URL --username admin --password $ARGO_PASSWORD --grpc-web
-
+#
 argocd app create backend --repo https://github.com/vikramdevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/backend.yaml --sync-policy auto --grpc-web
-argocd app create frontend --repo https://github.com/vikramdevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/frontend.yaml --sync-policy auto --grpc-web
+#argocd app create frontend --repo https://github.com/vikramdevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/frontend.yaml --sync-policy auto --grpc-web
