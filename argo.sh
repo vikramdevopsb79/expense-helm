b0ba7e7475c07a08a25c91991625faa69ba9c15c
sai <kuppalasaivikram@gmail.com>
2024-07-16 18:10:01 +0530

    rds module is created

--- argo.sh
+++ argo.sh
@@ -1,5 +1,5 @@
-#ARGO_URL=$(kubectl get svc argocd-server -n argocd| grep argocd-server | awk '{print $4}')
-ARGO_URL=argocd-dev.vikramdevops.tech
+ARGO_URL=$(kubectl get svc argocd-server -n argocd| grep argocd-server | awk '{print $4}')
+#ARGO_URL=argocd-dev.vikramdevops.tech
 ARGO_PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json  | jq '.data.password' | xargs | base64 --decode)
 
 argocd login $ARGO_URL --username admin --password $ARGO_PASSWORD --grpc-web
