5f5a457f30f9a7078c841f4a1e64e525acef6a68
sai <kuppalasaivikram@gmail.com>
2024-07-18 15:10:35 +0530

    n

--- argo.sh
+++ argo.sh
@@ -3,6 +3,6 @@ ARGO_URL=$(kubectl get svc argocd-server -n argocd| grep argocd-server | awk '{p
 ARGO_PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json  | jq '.data.password' | xargs | base64 --decode)
 
 argocd login $ARGO_URL --username admin --password $ARGO_PASSWORD --grpc-web
-#
+
 argocd app create backend --repo https://github.com/vikramdevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/backend.yaml --sync-policy auto --grpc-web
-#argocd app create frontend --repo https://github.com/vikramdevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/frontend.yaml --sync-policy auto --grpc-web
+argocd app create frontend --repo https://github.com/vikramdevopsb79/expense-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/frontend.yaml --sync-policy auto --grpc-web
